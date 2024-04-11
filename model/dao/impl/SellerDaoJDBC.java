1175fa84b4a40ba8bcc56b0435336ae8bcc8c347
LRCalamari <Lrcalamari.06@gmail.com>
2024-04-11 11:16:21 -0300

    update implementation

--- model/dao/impl/SellerDaoJDBC.java
+++ model/dao/impl/SellerDaoJDBC.java
@@ -13,6 +13,7 @@ import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
+
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -24,7 +25,7 @@ public class SellerDaoJDBC implements SellerDao {
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-
+	
 	@Override
 	public void insert(Seller obj) {
 		PreparedStatement st = null;
@@ -35,15 +36,15 @@ public class SellerDaoJDBC implements SellerDao {
 					+ "VALUES "
 					+ "(?, ?, ?, ?, ?)",
 					Statement.RETURN_GENERATED_KEYS);
-
+			
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
-
+			
 			int rowsAffected = st.executeUpdate();
-
+			
 			if (rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				if (rs.next()) {
@@ -67,13 +68,35 @@ public class SellerDaoJDBC implements SellerDao {
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ?");
+
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
+
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
 	}
+	
 	@Override
 	public Seller findById(Integer id) {
 		PreparedStatement st = null;
