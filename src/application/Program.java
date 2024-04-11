0f59f464afb340ccba43819ac884e3501dcd3720
LRCalamari <Lrcalamari.06@gmail.com>
2024-04-11 10:52:00 -0300

    findByld implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,18 @@
 
-import java.util.Date;
+package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
-public static void main(String[] args) {
+public class Program {
 
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+	public static void main(String[] args) {
+
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		Seller seller = sellerDao.findById(3);
 
 		System.out.println(seller);
 	}
